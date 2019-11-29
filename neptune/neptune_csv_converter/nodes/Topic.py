a34fa630372db299d50467e678a1b51ca6d1103b
Wang <angelaw@amazon.com>
2019-11-29 08:17:39 -0800

    fix typo

--- neptune/neptune_csv_converter/nodes/Topic.py
+++ neptune/neptune_csv_converter/nodes/Topic.py
@@ -5,9 +5,9 @@ class Topic(Node):
     def __init__(self, name):
         super().__init__('topic', name)
         self._terms = {}
-        self._terms_weight = {}
+        self._term_weight = {}
 
     def have_weighted_term(self, term, weight):
         self._terms[term.name] = term
-        self._terms_weight[term.name] = weight
+        self._term_weight[term.name] = weight
         return term, weight
